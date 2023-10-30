0dec2ba4089bf5462b8e7b846dd2f930df173ba7
AidanLDev <dev@aidanlowson.com>
2023-10-30 21:39:12 +0000

    Getting the app working

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandAloneServer } from '@apollo/server/standalone';
-
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { games, reviews, authors } from './_db.js';
 // types
-import { typeDefs } from './schema';
+import { typeDefs } from './schema.js';
 
 const port = 4000;
 
+const resolvers = {
+  Query: {
+    games() {
+      return games;
+    },
+    reviews() {
+      return reviews;
+    },
+    authors() {
+      return authors;
+    },
+  },
+};
+
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
 });
 
-const { url } = await startStandAloneServer(server, {
+const { url } = await startStandaloneServer(server, {
   listen: { port: port },
 });
 
-console.log(`Server ready at port ${port}`);
+console.log(`Server ready at port ${url}`);
