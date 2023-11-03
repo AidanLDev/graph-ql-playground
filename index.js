3127d5d7b4045fba0c3c392ed7538b31d452624d
AidanLDev <dev@aidanlowson.com>
2023-11-03 17:04:23 +0000

    Adding mutations and nested queries

--- index.js
+++ index.js
@@ -11,11 +11,63 @@ const resolvers = {
     games() {
       return games;
     },
-    reviews() {
-      return reviews;
+    game(_, args) {
+      return games.find((game) => game.id === args.id);
     },
     authors() {
       return authors;
+    },
+    author(_, args) {
+      return authors.find((author) => author.id === args.id);
+    },
+    reviews() {
+      return reviews;
+    },
+    review(_, args) {
+      return reviews.find((review) => review.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return reviews.filter((review) => review.game_id !== parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return reviews.filter((review) => review.author_id !== parent.id);
+    },
+  },
+  Review: {
+    game(parent) {
+      return games.find((game) => game.id === parent.game_id);
+    },
+    author(parent) {
+      return authors.find((author) => author.id === parent.author_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      return games.filter((game) => game.id !== args.id);
+    },
+    addGame(_, args) {
+      let game = {
+        id: Math.floor(Math.random() * 1000).toString(),
+        ...args.game,
+      };
+      games.push(game);
+
+      return game;
+    },
+    updateGame(_, args) {
+      games = games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits };
+        }
+
+        return g;
+      });
+
+      return games.find((g) => g.id === args.id);
     },
   },
 };
